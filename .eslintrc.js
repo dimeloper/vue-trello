6d92d11c9692b223581f79ea8b85c67276d2bba7
Dimitris Kiriakakis <dkiriakakis@gmail.com>
2020-03-14 13:25:21 +0000

    Fix postcss, board initial setup.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/standard'],
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#bulb-rules
+    'plugin:vue/recommended',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    // https://github.com/prettier/eslint-config-prettier
+    'prettier',
+    'prettier/standard',
+    'prettier/vue',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="board"> </div>
+  <div class="board">
+    <div class="flex flex-row items-start">
+      <div v-for="(column, i) of board.columns" :key="i" class="column">
+        <div class="flex items-center mb-2 font-bold">
+          {{ column.name }}
+        </div>
+        <div class="list-reset">
+          <div v-for="(task, j) of column.tasks" :key="j" class="task">
+            <span class="w-full flex-no-shrink font-bold">
+              {{ task.name }}
+            </span>
+            <p
+              v-if="task.description"
+              class="w-full flex-no-shrink mt-1 text-sm"
+            >
+              {{ task.description }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import {mapState} from 'vuex';
+
+export default {
+  computed: mapState(['board']),
+};
 </script>
 
 <style lang="css">
